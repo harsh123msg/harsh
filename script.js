3d4b2d141d75463b44b8cc5b253693c510617e34
harsh123msg <156303478+harsh123msg@users.noreply.github.com>
2024-02-09 21:12:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("btn").addEventListener("click", function() {
+    var url = "main.html";
+    if (url !== "") 
+    window.location.href = url;
+    
+  });
+
+document.getElementById("btn_play").addEventListener("click", function() {
+    var url = "TicTacToeDescription.html";
+    if (url !== "") 
+    window.location.href = url;
+    
+  });
